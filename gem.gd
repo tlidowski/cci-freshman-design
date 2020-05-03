bb35a01de11d45107e7245ff9d0ffb9e9fe5638c
Adrienne Cheung <amc669@drexel.edu>
2020-05-02 22:45:42 -0400

    armor files

--- gem.gd
+++ gem.gd
@@ -1,5 +1,7 @@
 extends Area2D
+# Called when the node enters the scene tree for the first time.
 
+		#Replace with function body.
 func _on_Gem_area_entered(area):
 	if "Player" in area.name:
 		queue_free()
