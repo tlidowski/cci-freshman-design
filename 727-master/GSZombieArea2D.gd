0db649d5431f8a953c305e6ca8036d0964d7dc84
Shirley Wu <sw3343@drexel.edu>
2020-06-05 12:19:48 -0400

    clearing

--- 727-master/GSZombieArea2D.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-const damage = -5
-
-
-func _ready():
-	pass 
--- 727-master/bubble.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-func _on_bubble_area_entered(area):
-	if "Player" in area.name:
-		GlobalVars.bubbleobtained()
-		queue_free()
