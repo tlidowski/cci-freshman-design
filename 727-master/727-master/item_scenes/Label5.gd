8a4257366253c736ccecdf586b1c3befc405d5a5
Shirley Wu <sw3343@drexel.edu>
2020-06-08 19:36:13 -0400

    clear

--- 727-master/727-master/item_scenes/Label5.gd
+++
@@ -1,7 +0,0 @@
-extends Label
-
-func _ready():
-	GlobalVars.connect("zombiesignal", self, "_ready")
-	GlobalVars.connect("gemsignal", self, "_ready")
-	GlobalVars.connect("zcountsignal", self, "_ready")
-	self.set_text("Gems Collected: {0}/{2}\nZombies Killed: {1}".format({"0": str(int(GlobalVars.gem_count)), "1":str(int(GlobalVars.zombie_kills)), "2":str(int(GlobalVars.zombies))}))
--- 727-master/727-master/zombie_scenes/GSZombieArea2D.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-const damage = -5
-
-func _ready():
-	pass 
