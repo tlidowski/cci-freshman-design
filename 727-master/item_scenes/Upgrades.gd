71a509846f3fa389ee5802d1e27a2fba228bf27e
Adrienne Cheung <amc669@drexel.edu>
2020-06-09 19:40:57 -0400

    Explained how dropped items' type and position are randomly determined

--- 727-master/item_scenes/Upgrades.gd
+++ 727-master/item_scenes/Upgrades.gd
@@ -6,16 +6,22 @@
 
 extends KinematicBody2D
 
+#Preloads orb and bubble scenes so they can be instanced later in code
 const ORB = preload("res://item_scenes/Orb.tscn")
 const BUBBLE = preload("res://item_scenes/bubble.tscn")
 
+#Randomly determines the type and in-game position of item intermittently dropped through utilization of random number generators.
 func _physics_process(delta):
+	
+	#Random number generator is randomized to determine whether orb or bubble is dropped through utilization of resulting variable "spawnitem"
 	var spawn = RandomNumberGenerator.new()
 	spawn.randomize()
 	var spawnitem = spawn.randi_range(1, 5000)
 	if spawnitem == 4148 or spawnitem == 1219 or spawnitem == 1131:
+		#instances orb, makes sure that orb does not disappear even if current node does by adding orb as child of current node's parent.
 		var orb = ORB.instance()
 		get_parent().add_child(orb)
+		#Another random number generator is randomized to obtain integer to add to current x position and integer to add to current y position
 		var posadd = RandomNumberGenerator.new()
 		posadd.randomize()
 		var posadded = posadd.randi_range(2, 600)
@@ -23,8 +29,10 @@ func _physics_process(delta):
 		posadded = posadd.randi_range(60, 300)
 		orb.position.y = self.position.y + posadded
 	elif spawnitem == 3060:
+		#instances orb, makes sure that orb does not disappear even if current node does by adding orb as child of current node's parent.
 		var bubble = BUBBLE.instance()
 		get_parent().add_child(bubble)
+		#Another random number generator is randomized to obtain integer to add to current x position and integer to add to current y position
 		var posadd = RandomNumberGenerator.new()
 		posadd.randomize()
 		var posadded = posadd.randi_range(2, 600)
